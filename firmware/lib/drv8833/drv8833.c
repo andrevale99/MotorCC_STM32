b21546f56f565ed27c4c93a27037eed48f75188a
andrevale99 <andrevale@gmail.com>
2026-09-10 19:23:17 -0300

    drv8833:biblioteca cirada

---
+++ firmware/lib/drv8833/drv8833.c
@@ -0,0 +1,47 @@
+#include "drv8833.h"
+
+static uint32_t max_duty_cycle = 0;
+
+drv8833_err_t drv8833_init(void (*configure_peripherals)(void),
+                           drv8833_motor_t *motor)
+{
+
+    if (configure_peripherals == NULL)
+    {
+        return DRV8833_ERROR_NO_CONFIGURE;
+    }
+
+    if (motor->set_ain == NULL || motor->set_bin == NULL)
+    {
+        return DRV8833_ERROR_INVALID_CHANNEL_FUNCTIONS;
+    }
+
+    if (motor->max_dutycycle == 0)
+    {
+        return DRV8833_ERROR_INVALID_DUTYCYCLE;
+    }
+
+    configure_peripherals();
+
+    return DRV8833_OK;
+}
+
+drv8833_err_t drv8833_set_motor_dutycycle(drv8833_motor_t *motor,
+                                          uint32_t dutycycle_ain, 
+                                          uint32_t dutycycle_bin)
+{
+    if (motor == NULL)
+    {
+        return DRV8833_ERROR_MOTOR_NULL;
+    }
+
+    if (dutycycle_ain > motor->max_dutycycle || dutycycle_bin > motor->max_dutycycle)
+    {
+        return DRV8833_ERROR_INVALID_DUTYCYCLE;
+    }
+
+    motor->set_ain(dutycycle_ain);
+    motor->set_bin(dutycycle_bin);
+
+    return DRV8833_OK;
+}
---
+++ firmware/lib/drv8833/drv8833.h
@@ -0,0 +1,36 @@
+#ifndef DRV8833_H
+#define DRV8833_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include <stm32f411xe.h>
+
+typedef enum
+{
+    DRV8833_OK = 0,
+    DRV8833_ERROR_NO_CONFIGURE = -1,
+    DRV8833_ERROR_INVALID_CHANNEL_FUNCTIONS = -2,
+    DRV8833_ERROR_INVALID_DUTYCYCLE = -3,
+    DRV8833_ERROR_MOTOR_NULL = -4
+} drv8833_err_t;
+
+typedef struct
+{
+    void (*set_ain)(uint32_t dutycycle);
+    uint32_t dutycycle_ain;
+
+    void (*set_bin)(uint32_t dutycycle);
+    uint32_t dutycycle_bin;
+
+    const uint32_t max_dutycycle;
+} drv8833_motor_t;
+
+drv8833_err_t drv8833_init(void (*configure_peripherals)(void),
+                           drv8833_motor_t *motor);
+
+drv8833_err_t drv8833_set_motor_dutycycle(drv8833_motor_t *motor,
+                                          uint32_t dutycycle_ain, 
+                                          uint32_t dutycycle_bin);
+
+#endif
