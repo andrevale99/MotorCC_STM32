8bc3be6beced8521c72e5a9d27ef097266ad5aa1
andrevale99 <andrevale@gmail.com>
2026-09-14 11:05:08 -0300

    main:update

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-#include <stm32f411xe.h>
-
-#include "drv8833_startup.h"
-#include "encoder_startup.h"
+#include "system_motor.h"
+#include "system_uart.h"
 
 #include "rcc.h"
 #include "uart.h"
 #include "timer.h"
-#include "motor.h"
-#include "encoder.h"
-#include "drv8833.h"
-
-char buffer[64] = {0};
 
 int main(void)
 {
+
     rcc_confg_t config = {
         .clock_source = RCC_HSE,
         .ahb_divider = RCC_AHB_DIV_0,
@@ -36,48 +30,8 @@ int main(void)
         GPIOC->BSRR |= GPIO_BSRR_BR13; // Clock source is HSE
     }
 
-    usart1_init(SystemCoreClock, 115200U);
-
-    drv8833_t motor = {
-        .set_ain = motor_set_ain,
-        .set_bin = motor_set_bin,
-        .set_sleep = motor_set_sleep,
-        .max_dutycycle = 1000,
-    };
-
-    timer_err_t rettimer = TIMER_OK;
-    rettimer = timer_use(motor_configure_peripherals, TIMER_2_32_BIT);
-    if (rettimer != TIMER_OK)
-    {
-        sprintf(buffer, "Erro oa iniciar timer do drv8833: %i\n",
-                rettimer);
-        usart1_send_string(buffer);
-        return -1;
-    }
-    sprintf(buffer, "timer do drv8833 OK: %i\n",
-            rettimer);
-    usart1_send_string(buffer);
-
-    drv8833_init(motor_configure_peripherals, &motor);
-    drv8833_set_sleep(&motor, 1); // Wake up the motor driver
-
-    rettimer = timer_use(encoder_configure_peripherals, TIMER_4_16_BIT);
-    if (rettimer != TIMER_OK)
-    {
-        sprintf(buffer, "Erro oa iniciar timer do encoder: %i\n",
-                rettimer);
-        usart1_send_string(buffer);
-        return -1;
-    }
-    sprintf(buffer, "timer do encoder OK: %i\n",
-            rettimer);
-    usart1_send_string(buffer);
-    encoder_configure_peripherals();
-
-#ifdef MOTOR_TESTE
-        #include "../test/motor_teste.h"
-        motor_teste(buffer, &motor);
-#endif
+    system_uart_init();
+    system_motor_init();
 
     return 0;
 }
