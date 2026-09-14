dbfecc71dfe06f76b98330d117d2afc0e77e54d7
andrevale99 <andrevale@gmail.com>
2026-09-14 11:55:52 -0300

    main:update

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,35 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "system_motor.h"
+#include "system_rcc.h"
 #include "system_uart.h"
-
-#include "rcc.h"
-#include "uart.h"
-#include "timer.h"
+#include "system_motor.h"
 
 int main(void)
 {
 
-    rcc_confg_t config = {
-        .clock_source = RCC_HSE,
-        .ahb_divider = RCC_AHB_DIV_0,
-        .apb1_divider = RCC_APBx_DIV_0,
-        .apb2_divider = RCC_APBx_DIV_0};
-
-    rcc_init(&config);
-
-    rcc_clock_status_t status;
-    rcc_get_clock_status(&status);
-    SystemCoreClockUpdate();
-
-    if (status.clock_source == RCC_HSE)
-    {
-        RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN; // Enable GPIOC clock
-        GPIOC->MODER |= GPIO_MODER_MODER13_0;
-        GPIOC->BSRR |= GPIO_BSRR_BR13; // Clock source is HSE
-    }
-
+    system_rcc_init();
     system_uart_init();
     system_motor_init();
 
