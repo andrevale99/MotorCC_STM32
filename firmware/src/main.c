b7f30760d6d2e26fccb794b8dbf617dba012ccf3
andrevale99 <andrevale@gmail.com>
2026-09-15 10:00:46 -0300

    main:update

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,18 +5,20 @@
 #include "system_uart.h"
 #include "system_motor.h"
 
-int main(void)
+void system_init(void)
 {
-
     system_rcc_init();
     system_uart_init();
     system_motor_init();
+}
 
-    while(1)
+int main(void)
+{
+    system_init();
+
+    while (1)
     {
-        log_info("PULSOS: %i", get_pulse());
-        for(int i=0; i < 25000000U / 100; i++)
-            ;
+        system_motor_loop();
     }
 
     return 0;
